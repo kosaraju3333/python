2d26dde659096e9b1d3a382d5775585c8c080260
kosaraju3333 <kosaraju3333@gmail.com>
2023-12-30 12:59:36 +0530

    added loops in python

---
+++ loops/for_loop.py
@@ -0,0 +1,17 @@
+fruits = ["Mango", "Apple", "Banana"]
+
+# For loop
+for x in fruits:
+    print(x)
+
+# Break statement
+for x in fruits:
+    if x == "Apple":
+        break
+    print(x)
+
+# Continue Statement
+for x in fruits:
+    if x == "Apple":
+        continue
+    print(x)
---
+++ loops/while_loop.py
@@ -0,0 +1,6 @@
+n = 10
+count = 0
+while count < n:
+    print(count)
+    count += 1
+
