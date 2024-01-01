c814d19a5fd9e32637b37c4302c8aceb98bb7b06
kosaraju3333 <kosaraju3333@gmail.com>
2024-01-01 16:58:03 +0530

    List of files in folders usig fucntions

---
+++ scripts/list_files_in_folders_using_functions.py
@@ -0,0 +1,33 @@
+## This Program is to list the files from the folders using functions & try and except 
+
+import os
+
+def list_files_in_folder(folder_path):
+    try:
+        files = os.listdir(folder_path)
+        return files, None
+    except FileNotFoundError:
+        return None, "Folder not found"
+    except PermissionError:
+        return None, "Permission denied"
+
+def main():
+    folder_paths = input("Enter a list of folder paths separated by spaces: ").split()
+    
+    for folder_path in folder_paths:
+        # Below Declearing variable a and b and accesing return values from list_files_in_folder(folder_path) function 
+        a, b = list_files_in_folder(folder_path)
+        print("a Value: ", a)
+        print("b Value: ", b)
+        if a:
+            print("Files in: ",folder_path)
+            for file in a:
+                print(file)
+        else:
+            print(f"Error in {folder_path}: {b}")
+
+if __name__ == "__main__":
+    main()
+
+
+
