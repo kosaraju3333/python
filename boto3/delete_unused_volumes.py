d2495bfbc09ce5311f2ccd054afb0f902fdd5132
kosaraju3333 <kosaraju3333@gmail.com>
2024-02-06 16:53:54 +0530

    added boto script to delete un attached volumes

---
+++ boto3/delete_unused_volumes.py
@@ -0,0 +1,18 @@
+import boto3
+from pprint import pprint
+
+## Open AWS console session
+iam_console = boto3.Session(profile_name='boto3-user')
+
+## Open EC2 Console
+ec2_console_client = iam_console.client(service_name='ec2', region_name='us-east-1')
+
+## ec2 volumes client method
+list_volumes = ec2_console_client.describe_volumes()['Volumes']
+for volumes in list_volumes:
+    volume_id = volumes['VolumeId']
+    if volumes['State'] == 'available':
+        print('Deleting Volume....: ', volume_id)
+        ec2_console_client.delete_volume(VolumeId = volume_id)
+        print('Deleted Volume: ', volume_id)
+        
