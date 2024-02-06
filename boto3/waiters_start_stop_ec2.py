f3c4887260064a66a3883757181f77b56cd828c0
kosaraju3333 <kosaraju3333@gmail.com>
2024-02-06 11:48:11 +0530

    added waiters stop start ec2 script

---
+++ boto3/waiters_start_stop_ec2.py
@@ -0,0 +1,35 @@
+import boto3
+
+## Openong AWS console
+aws_console = boto3.Session(profile_name='boto3-user')
+
+## Opening ec2 Service/Client
+ec2_console_client = aws_console.client(service_name='ec2', region_name='us-east-1')
+
+############# Start Instance ##################
+print('Starting Ec2 Instances')
+## ec2 instance start clinet Methods 
+instance_start = ec2_console_client.start_instances(
+    InstanceIds=['i-0439d8a96d4dec453']
+)
+## Creating a waiter
+instance_start_waiter = ec2_console_client.get_waiter('instance_running')
+## Waiter Method
+instance_start_waiter.wait(
+    InstanceIds=['i-0439d8a96d4dec453']
+)
+print("Ec2 instance started")
+
+############ Stop Instance #############
+print('Ec2 instance Stopping ')
+## ec2 instance stop Client Method
+instance_stop = ec2_console_client.stop_instances(
+    InstanceIds=['i-0439d8a96d4dec453']
+)
+## Create Waiter
+instance_stop_waiter = ec2_console_client.get_waiter('instance_stopped')
+## Waiter Method
+instance_stop_waiter.wait(
+    InstanceIds=['i-0439d8a96d4dec453']
+)
+print('Ec2 instance Stopped')
